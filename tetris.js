5ebfa63e79888cd4c0eaad5cf7185ed93c4ad589
xeviert <69497859+xeviert@users.noreply.github.com>
2020-10-26 16:15:13 -0500

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -5,7 +5,7 @@ context.scale(20, 20);
 
 function arenaSweep() {
     let rowCount = 1;
-    outer:  for (let y = arena.length -1; y < 0; --y) {
+    outer:  for (let y = arena.length -1; y > 0; --y) {
                 for (let x = 0; x < arena[y].length; ++x) {
                     if (arena[y][x] === 0) {
                         continue outer;
